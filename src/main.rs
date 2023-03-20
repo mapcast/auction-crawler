4ab945e23927ddcb58fb0eb317b4eba3a98e7cb4
mapcast <odri0220@naver.com>
2023-03-21 00:20:44 +0900

    postgre 추가, 2주 뒤 문자열 추가

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{io::Read, process::Command};
 use encoding::{DecoderTrap, label::encoding_from_whatwg_label};
+use chrono::{Duration, Datelike};
+use postgres::{Client, NoTls};
 
 fn hex2str(u8vec: &[u8]) -> &str {
     let euckr = encoding_from_whatwg_label("euc-kr").unwrap();
@@ -13,16 +15,40 @@ fn string_to_static_str(s: String) -> &'static str {
 
 
 fn main() {
+
+    let mut client = Client::connect("host=localhost user=postgres", NoTls).unwrap();
+
+    for row in client.query("SELECT id, name FROM TEST_TABLE", &[]).unwrap() {
+        let id: i32 = row.get(0);
+        let name: &str = row.get(1);
+    
+        println!("found person: {} {}", id, name);
+    }
+
     //let query = r#"curl https://www.courtauction.go.kr/RetrieveRealEstMulDetailList.laf -d srnID=PNO102000&jiwonNm=%BE%C8%BB%EA%C1%F6%BF%F8&bubwLocGubun=1&jibhgwanOffMgakPlcGubun=&mvmPlaceSidoCd=&mvmPlaceSiguCd=&roadPlaceSidoCd=&roadPlaceSiguCd=&daepyoSidoCd=&daepyoSiguCd=&daepyoDongCd=&rd1Cd=&rd2Cd=&rd3Rd4Cd=&roadCode=&notifyLoc=1&notifyRealRoad=1&notifyNewLoc=1&mvRealGbncd=1&jiwonNm1=%BE%C8%BB%EA%C1%F6%BF%F8&jiwonNm2=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&mDaepyoSidoCd=&mvDaepyoSidoCd=&mDaepyoSiguCd=&mvDaepyoSiguCd=&realVowel=00000_55203&vowelSel=00000_55203&mDaepyoDongCd=&mvmPlaceDongCd=&_NAVI_CMD=&_NAVI_SRNID=&_SRCH_SRNID=PNO102000&_CUR_CMD=RetrieveMainInfo.laf&_CUR_SRNID=PNO102000&_NEXT_CMD=RetrieveRealEstMulDetailList.laf&_NEXT_SRNID=PNO102002&_PRE_SRNID=PNO102001&_LOGOUT_CHK=&_FORM_YN=Y"#;
-    let query = r#"curl https://www.courtauction.go.kr/RetrieveRealEstMulDetailList.laf -d "page=default20&srnID=PNO102000&jiwonNm=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&bubwLocGubun=1&jibhgwanOffMgakPlcGubun=&mvmPlaceSidoCd=&mvmPlaceSiguCd=&roadPlaceSidoCd=&roadPlaceSiguCd=&daepyoSidoCd=&daepyoSiguCd=&daepyoDongCd=&rd1Cd=&rd2Cd=&rd3Rd4Cd=&roadCode=&notifyLoc=1&notifyRealRoad=1&notifyNewLoc=1&mvRealGbncd=1&jiwonNm1=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&jiwonNm2=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&mDaepyoSidoCd=&mvDaepyoSidoCd=&mDaepyoSiguCd=&mvDaepyoSiguCd=&realVowel=00000_55203&vowelSel=00000_55203&mDaepyoDongCd=&mvmPlaceDongCd=&_NAVI_CMD=&_NAVI_SRNID=&_SRCH_SRNID=PNO102000&_CUR_CMD=RetrieveMainInfo.laf&_CUR_SRNID=PNO102000&_NEXT_CMD=&_NEXT_SRNID=PNO102002&_PRE_SRNID=PNO102001&_LOGOUT_CHK=&_FORM_YN=Y&PNIPassMsg=%C1%A4%C3%A5%BF%A1+%C0%C7%C7%D8+%C2%F7%B4%DC%B5%C8+%C7%D8%BF%DCIP+%BB%E7%BF%EB%C0%DA%C0%D4%B4%CF%B4%D9.&pageSpec=default20&pageSpec=default20&targetRow=1&lafjOrderBy=order+by+maeGiil+desc""#;
+    
+    let loc = chrono::Local::now() + Duration::days(14);
+    let year: String = loc.year().to_string();
+    let mut month: String = loc.month().to_string();
+    let mut day: String = loc.day().to_string();
+    println!("{}", month.len());
+    if month.len() == 1 {
+        month = format!("0{}", month);
+    }
+    if day.len() == 1 {
+        day = format!("0{}", day);
+    }
+
+    let search_date = format!("{}.{}.{}", year, month, day);
+    
+    let query = format!(r#"curl https://www.courtauction.go.kr/RetrieveRealEstMulDetailList.laf -d "page=default40&bubwLocGubun=1&jpDeptCd=000000&daepyoSidoCd=&daepyoSiguCd=&daepyoDongCd=&notifyLoc=on&rd1Cd=&rd2Cd=&realVowel=00000_55203&rd3Rd4Cd=&notifyRealRoad=on&saYear=2023&saSer=&ipchalGbncd=000331&lclsUtilCd=&mclsUtilCd=&sclsUtilCd=&gamEvalAmtGuganMin=&gamEvalAmtGuganMax=&notifyMinMgakPrcMin=&notifyMinMgakPrcMax=&areaGuganMin=&areaGuganMax=&yuchalCntGuganMin=&yuchalCntGuganMax=&notifyMinMgakPrcRateMin=&notifyMinMgakPrcRateMax=&srchJogKindcd=&mvRealGbncd=00031R&srnID=PNO102001&_NAVI_CMD=&_NAVI_SRNID=&_SRCH_SRNID=PNO102001&_CUR_CMD=InitMulSrch.laf&_CUR_SRNID=PNO102001&_NEXT_CMD=&_NEXT_SRNID=PNO102002&_PRE_SRNID=&_LOGOUT_CHK=&_FORM_YN=Y&PNIPassMsg=^%^C1^%^A4^%^C3^%^A5^%^BF^%^A1+^%^C0^%^C7^%^C7^%^D8+^%^C2^%^F7^%^B4^%^DC^%^B5^%^C8+^%^C7^%^D8^%^BF^%^DCIP+^%^BB^%^E7^%^BF^%^EB^%^C0^%^DA^%^C0^%^D4^%^B4^%^CF^%^B4^%^D9.&pageSpec=default20&pageSpec=default40&targetRow=201&lafjOrderBy=order+by+maeGiil+desc&termStartDt={}&termEndDt={}"#, search_date, search_date);
     let output = Command::new("cmd")
-            .args(["/C", query])
+            .args(["/C", &query])
             .output()
             .expect("failed to execute process");
     
     let response = hex2str(&output.stdout);
     println!("{}", response);
-    println!("{:?}", output);
+     
+    //println!("{:?}", output);
 }
-
- 
